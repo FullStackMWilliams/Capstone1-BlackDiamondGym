6b70511871242444f1a687e98ad18c6cb3ffe644
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-14 15:06:24 -0400

    (ง'̀-'́)ง Project structure done need to add classes and csv files

--- BlackDiamondGym/src/main/java/com/pluralsight/BlackDiamondGym.java
+++ BlackDiamondGym/src/main/java/com/pluralsight/BlackDiamondGym.java
@@ -1,4 +1,5 @@
 package com.pluralsight;
+import java.util.Scanner;
 
                              /* BLACK DIAMOND GYM */
 
---
+++ BlackDiamondGym/src/main/java/com/pluralsight/User.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+import java.util.Objects;
+
+public class User {
+    private String username;
+    private String password;
+    private String admin;
+}
+
+
