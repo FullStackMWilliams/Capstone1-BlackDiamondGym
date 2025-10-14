3143efc6287eeb724aaad4ccb31a3041f7c62e05
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-14 19:40:45 -0400

    ᕦ( ◡̀﹏◡́)ᕤ Getting started on FileManager class

--- BlackDiamondGym/src/main/java/com/pluralsight/FileManager.java
+++ BlackDiamondGym/src/main/java/com/pluralsight/FileManager.java
@@ -1,4 +1,39 @@
 package com.pluralsight;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class FileManager {
+
+    public static final String USERS_FILE = "users.csv";
+    public static final String MEMBERS_FILE = "members.csv";
+    public static final String TRANSACTIONS_FILE = "transactions.csv";
+
+    public static void ensureFiles() {
+        try {
+            createIfMissing(USERS_FILE);
+            createIfMissing(MEMBERS_FILE);
+            createIfMissing(TRANSACTIONS_FILE);
+
+
+        }catch (Exception e) {
+            System.out.println(Colors.Red + "File set up error: " + e.getMessage() +Colors.RESET);
+
+        }
+        }
+        public static void createIfMissing(String file) throws IOException {
+            Path path = Path.of(file);
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+        }
+        public
+
+
+        private static String sanitize(String s) {
+        if ( s == null) return "";
+        return s.replace(","," ");
+    }
 }
