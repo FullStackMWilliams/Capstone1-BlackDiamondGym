7b849c484708046e0a1d25d10f5fe0542e069642
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-14 16:00:52 -0400

    (̿▨-▨¬˵) home screen done... way to much time on colors

--- BlackDiamondGym/src/main/java/com/pluralsight/BlackDiamondGym.java
+++ BlackDiamondGym/src/main/java/com/pluralsight/BlackDiamondGym.java
@@ -1,26 +1,41 @@
 package com.pluralsight;
-import java.util.Scanner;
 
-                             /* BLACK DIAMOND GYM */
+/* BLACK DIAMOND GYM */
 
 
 public class BlackDiamondGym {
 
                     // Color theme for CLI application
 
-    public static final String Brown = "\u001b[33m";
-    public static final String Beige = "\u001b[37m";
-    public static final String Olive = "\u001b[32m";
-    public static final String Amber = "\u001b[33m";
-    public static final String Seafoam = "\u001b[36";
-    public static final String DeepBlue = "\u001b[34";
-    public static final String Aqua = "\u001b[96";
-    public static final String OceanBlue = "\u001b[94";
-    public static final String Gray = "\u001b[90";
-    public static final String RESET = "\u001b[0";
+    public static final String BeigeBackground = "\u001B[100m";
+    public static final String Purple = "\u001B[35m";
+    public static final String Olive = "\u001B[32m";
+    public static final String Amber = "\u001B[33m";
+    public static final String Seafoam = "\u001B[36m";
+    public static final String DeepBlue = "\u001B[34m";
+    public static final String Aqua = "\u001B[96m";
+    public static final String OceanBlue = "\u001B[94m";
+    public static final String Gray = "\u001B[90m";
+    public static final String RESET = "\u001B[0m";
 
 
     public static void main(String[] args) {
+        displayHomeScreen();
 
+    }
+
+    public static void displayHomeScreen() {
+        // clearScreen();
+
+        System.out.println(BeigeBackground + "================================="+ RESET);
+        System.out.println(Purple + " Welcome to BlackDiamond Gym" + RESET);
+        System.out.println(BeigeBackground + "=================================" + RESET);
+        System.out.println(Seafoam + "\nYour journey to strength and balance begins here." + RESET);
+        System.out.println(Gray + "\n-----------------------------------------------------" + RESET);
+        System.out.println(Olive + "[1]" + RESET + " Login");
+        System.out.println(Olive + "[2]" + RESET + " Sign Up");
+        System.out.println(Olive + "[3]" + RESET + " Exit");
+        System.out.println(Gray + "-------------------------------------------------------");
+        System.out.println(Amber + "\"Life is easier when you're stronger\".- Markus" + RESET);
     }
 }
