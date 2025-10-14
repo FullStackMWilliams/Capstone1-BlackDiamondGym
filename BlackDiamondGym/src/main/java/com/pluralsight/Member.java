634872b340fdd296a93e167cfdccea2e22255d80
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-14 16:53:23 -0400

    (^⇀ᴥ↼^) Members class complete

--- BlackDiamondGym/src/main/java/com/pluralsight/Member.java
+++ BlackDiamondGym/src/main/java/com/pluralsight/Member.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
-public class Member {
+public class Member extends User {
+    private Membership membership;
+
+    public Member() {
+        super();
+        setRole("MEMBER");
+    }
+    public Member(String username, String password) {
+        super(username,password, "MEMBER");
+    }
+    public Membership getMembership() {
+        return membership;
+    }
+    public void setMembership(Membership membership) {
+        this.membership = membership;
+    }
 }
