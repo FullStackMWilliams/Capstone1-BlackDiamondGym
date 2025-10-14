7dc995748004b2d9c561ff3d7d865cc38a259cc4
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-14 15:20:54 -0400

    (・_・)ノ Color theme finished

--- BlackDiamondGym/src/main/java/com/pluralsight/BlackDiamondGym.java
+++ BlackDiamondGym/src/main/java/com/pluralsight/BlackDiamondGym.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 
 
 public class BlackDiamondGym {
+
+                   // Color theme for CLI application
+
+    public static final String Brown = "\u001b[33m";
+    public static final String Beige = "\u001b[37m";
+    public static final String Olive = "\u001b[32m";
+    public static final String Amber = "\u001b[33m";
+    public static final String Seafoam = "\u001b[36";
+    public static final String DeepBlue = "\u001b[34";
+    public static final String Aqua = "\u001b[96";
+    public static final String OceanBlue = "\u001b[94";
+    public static final String Gray = "\u001b[90";
+    public static final String RESET = "\u001b[0";
+
+
     public static void main(String[] args) {
 
     }
